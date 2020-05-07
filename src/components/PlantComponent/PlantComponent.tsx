98f4262ddc6e81204e695fc1433600877975784a
Nicolas Eckhart <nicolaseckhart@hotmail.com>
2020-05-07 12:35:34 +0200

    Turn plantcomponent into a class component

--- src/components/PlantComponent/PlantComponent.tsx
+++ src/components/PlantComponent/PlantComponent.tsx
@@ -3,23 +3,32 @@ import PlantState from '../../models/PlantState';
 import GrowthProgram from '../../models/GrowthProgram';
 import moment from 'moment';
 import { Row, Col, Button, Modal, Form } from 'react-bootstrap';
-import { useState } from 'react';
 import { PlantGraphic } from './PlantGraphic';
 
-export const PlantComponent: React.FC = () => {
-  const plantState = new PlantState();
-  const [show, setShow] = useState(false);
+interface State {
+  plantState: PlantState;
+  modalShown: boolean;
+}
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+export class PlantComponent extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      plantState: new PlantState(),
+      modalShown: false,
+    };
+  }
 
-  const renderGrowthProgramInfo = () => (
+  handleClose = () => this.setState({ modalShown: false });
+  handleShow = () => this.setState({ modalShown: true });
+
+  renderGrowthProgramInfo = () => (
     <Row>
       <Col className="text-center">
         <p className="text-light">
           <b>Plant Date:</b>
           <br />
-          {plantState.getPlantDate()}
+          {this.state.plantState.getPlantDate()}
         </p>
       </Col>
       <Col className="text-center">
@@ -39,52 +48,52 @@ export const PlantComponent: React.FC = () => {
     </Row>
   );
 
-  const renderPlantInputField = (num: number, value: string) => (
+  renderPlantInputField = (num: number, value: string) => (
     <Form.Group as={Col} controlId={`plant${num}`}>
       <Form.Label>Plant {num}</Form.Label>
       <Form.Control type="text" placeholder={`Plant ${num}`} value={value} disabled />
     </Form.Group>
   );
 
-  return (
+  render = () => (
     <div className="mt-5">
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={this.state.modalShown} onHide={this.handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Edit Values</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form.Row>
-            {renderPlantInputField(1, plantState.plant1)}
-            {renderPlantInputField(2, plantState.plant2)}
+            {this.renderPlantInputField(1, this.state.plantState.plant1)}
+            {this.renderPlantInputField(2, this.state.plantState.plant2)}
           </Form.Row>
 
           <Form.Row>
-            {renderPlantInputField(3, plantState.plant3)}
-            {renderPlantInputField(4, plantState.plant4)}
+            {this.renderPlantInputField(3, this.state.plantState.plant3)}
+            {this.renderPlantInputField(4, this.state.plantState.plant4)}
           </Form.Row>
 
           <Form.Group controlId="plantDate">
             <Form.Label>Plant Date</Form.Label>
-            <Form.Control placeholder="Plant Date" value={plantState.getPlantDate()} disabled />
+            <Form.Control placeholder="Plant Date" value={this.state.plantState.getPlantDate()} disabled />
           </Form.Group>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={this.handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={this.handleClose}>
             Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
 
-      {renderGrowthProgramInfo()}
+      {this.renderGrowthProgramInfo()}
 
-      <PlantGraphic plantState={plantState} />
+      <PlantGraphic plantState={this.state.plantState} />
 
-      <div className="nw-button" onClick={handleShow}>
+      <div className="nw-button" onClick={this.handleShow}>
         <span className="jam jam-cog" />
       </div>
     </div>
   );
-};
+}
