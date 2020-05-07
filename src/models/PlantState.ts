f0fc190139c91d177c30b11a9e7bfb0530887dd8
Nicolas Eckhart <nicolaseckhart@hotmail.com>
2020-05-07 11:34:27 +0200

    Display plant date and current week in program

--- src/models/PlantState.ts
+++ src/models/PlantState.ts
@@ -1,13 +1,21 @@
+import moment, { Moment } from 'moment';
+
 export default class PlantState {
   plant1: string;
   plant2: string;
   plant3: string;
   plant4: string;
+  private plantDate: Moment;
 
   constructor() {
     this.plant1 = 'Plant 1 Name';
     this.plant2 = 'Plant 2 Name';
     this.plant3 = 'Plant 3 Name';
     this.plant4 = 'Plant 4 Name';
+    this.plantDate = moment('2019-11-25');
+  }
+
+  getPlantDate() {
+    return this.plantDate.format("dddd, MMMM Do YYYY");
   }
 }
